73311ebb5a99a2af27aff73e3b575a54aad71a76
vedasharma451 <vedavissavajjula@gmail.com>
2026-03-16 14:20:44 +0530

    fix sqlite for render deploy

--- backend/database.js
+++ backend/database.js
@@ -2,31 +2,31 @@ const db = require("./models/db");
 
 const dentists = [
   {
-    name: "Dr. Swapna",
+    name: "Dr.Swapna",
     qualification: "BDS",
     experience: 4,
     clinic: "Swapna Dental",
     address: "kothapett Road",
     location: "Hyderabad",
-    photo: "https://i.pravatar.cc/150?img=12"
+    photo: "https://img.freepik.com/free-photo/beautiful-young-female-doctor-looking-camera-office_1301-7807.jpg?semt=ais_hybrid&w=740&q=80"
   },
   {
-    name: "Dr. Anjali Mehta",
+    name: "Dr.Adithya",
     qualification: "BDS, MDS",
-    experience: 12,
-    clinic: "Bright Teeth Clinic",
-    address: "Banjara Hills",
+    experience: 7,
+    clinic: "Adithya Teeth Clinic",
+    address: "Dilshuknagara",
     location: "Hyderabad",
-    photo: "https://i.pravatar.cc/150?img=32"
+    photo: "https://static.vecteezy.com/system/resources/thumbnails/026/375/249/small/ai-generative-portrait-of-confident-male-doctor-in-white-coat-and-stethoscope-standing-with-arms-crossed-and-looking-at-camera-photo.jpg"
   },
   {
-    name: "Dr. Kiran Patel",
+    name: "Dr.Arjun",
     qualification: "BDS",
-    experience: 6,
-    clinic: "Healthy Smile Center",
+    experience: 13,
+    clinic: "Arjun Center",
     address: "Kukatpally",
     location: "Hyderabad",
-    photo: "https://i.pravatar.cc/150?img=15"
+    photo: "https://i.pinimg.com/736x/4c/1c/9e/4c1c9edd3857a345d9c4868d7f1ce83d.jpg"
   }
 ];
 
